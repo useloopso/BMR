5ab9f47cc1b87c52f3d25b23ffb69e77cf2f76ba
lajosdeme <lajosd@protonmail.ch>
2023-11-22 00:16:18 +0100

    remove unused reflectValue utility function

--- utils/utils.go
+++ utils/utils.go
@@ -2,17 +2,8 @@ package utils
 
 import (
 	"bytes"
-	"reflect"
 )
 
 func EncodePacked(input ...[]byte) []byte {
 	return bytes.Join(input, nil)
-}
-
-func ToReflectValue(args []interface{}) []reflect.Value {
-	convertedArgs := make([]reflect.Value, len(args))
-	for i, arg := range args {
-		convertedArgs[i] = reflect.ValueOf(arg)
-	}
-	return convertedArgs
 }
